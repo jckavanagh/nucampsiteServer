482a859404923347d06501f74d38710e952207b3
Jessica Kavanagh <kavanagh.jc@gmail.com>
2023-12-20 14:06:30 -0600

    HTTP and Secure Communication

--- app.js
+++ app.js
@@ -28,12 +28,21 @@ connect.then(() => console.log('Connected correctly to server'),
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
